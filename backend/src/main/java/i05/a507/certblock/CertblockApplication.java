d11b13f1b84e443e70359b1782ec4dc44d0e05d5
hoonti06 <hoonti06@gmail.com>
2021-09-17 16:41:10 +0900

    feat : spring boot 프로젝트 추가

---
+++ backend/src/main/java/i05/a507/certblock/CertblockApplication.java
@@ -0,0 +1,13 @@
+package i05.a507.certblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertblockApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CertblockApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/i05/a507/certblock/CertblockApplicationTests.java
@@ -0,0 +1,13 @@
+package i05.a507.certblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertblockApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
