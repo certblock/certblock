06877960223597d8b5b0707337eb984f335d15ac
hoonti06 <hoonti06@gmail.com>
2021-10-04 14:20:09 +0900

    feat : 증명서 목록 조회 응답 데이터 변경

--- backend/src/main/java/i05/a507/certblock/dto/Student/StudentCertRes.java
+++ backend/src/main/java/i05/a507/certblock/dto/Student/StudentCertRes.java
@@ -3,10 +3,16 @@ package i05.a507.certblock.dto.Student;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class StudentCertRes {
     private int certificateId;
-    private int type; //증명서 종류
-    private boolean flg; //발급여부
+    private int studentId;
+    private int universityId;
+    private int type; // 증명서 종류
+    private boolean flg; // 발급 여부
+    private Date date; // 발급 날짜
+    private String hash;
 }
--- backend/src/main/java/i05/a507/certblock/dto/Student/StudentRegistUniReq.java
+++ backend/src/main/java/i05/a507/certblock/dto/Student/StudentRegistUniReq.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Setter
 public class StudentRegistUniReq {
     private int type;
+    private int studentIdInUniv;
 }
